ee3067289d802a0928be6f02dbc5ee85260f9843
Marco Alonso Rodriguez Tapia <mrodriguez@alquimiapay.com>
2023-11-15 09:48:16 -0600

    Getting Started

---
+++ TikTokClone/App/TikTokCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TikTokCloneApp.swift
+//  TikTokClone
+//
+//  Created by Marco Alonso Rodriguez on 15/11/23.
+//
+
+import SwiftUI
+
+@main
+struct TikTokCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
